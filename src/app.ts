3ec372ac42f20e1572f013a8b64750d84e015d8f
Tamir S. Ferreira <bigtam_rj@yahoo.com.br>
2023-05-22 18:29:04 -0300

    feat: create server base

---
+++ src/app.ts
@@ -0,0 +1,15 @@
+import express, { Application, json } from "express";
+import "express-async-errors";
+import "reflect-metadata";
+import { errorHandler } from "./errors";
+import * as routes from "./routes";
+
+const app: Application = express();
+app.use(json());
+
+app.use("/clients", routes.clients);
+app.use("/contacts", routes.contacts);
+
+app.use(errorHandler);
+
+export default app;
